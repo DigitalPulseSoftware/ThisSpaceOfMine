98abb9c987f1b6a9db553d01c5f1d17bc53247f3
SirLynix <lynix680@gmail.com>
2023-09-07 18:45:51 +0200

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,42 +1,3 @@
--- TEMPORARY
-
-package("perlinnoise")
-    set_kind("library", {headeronly = true})
-    set_homepage("https://github.com/Reputeless/PerlinNoise")
-    set_description("Header-only Perlin noise library for modern C++ (C++17/C++20)")
-    set_license("MIT")
-
-    add_urls("https://github.com/Reputeless/PerlinNoise/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/Reputeless/PerlinNoise.git")
-
-    add_versions("v3.0.0", "1fea1e7ebeb3c66b79d60c2c398aa83ccfadcef343bd396c0f0a684380e827fc")
-
-    on_install(function (package)
-        os.cp("PerlinNoise.hpp", package:installdir("include"))
-    end)
-
-    on_test(function (package)
-        assert(package:check_cxxsnippets([[
-            void foo()
-            {
-                const siv::PerlinNoise::seed_type seed = 123456u;
-                const siv::PerlinNoise perlin{ seed };
-                
-                for (int y = 0; y < 5; ++y)
-                {
-                    for (int x = 0; x < 5; ++x)
-                    {
-                        const double noise = perlin.octave2D_01((x * 0.01), (y * 0.01), 4);
-                    }
-                }
-            }
-        ]], {configs = {languages = "c++17"}, includes = "PerlinNoise.hpp"}))
-    end)
-package_end()
-
--- TEMPORARY
-
-
 -- Should the CommonLib be compiled statically? (takes more space)
 option("commonlib_static", { default = false, defines = "TSOM_COMMONLIB_STATIC"})
 option("clientlib_static", { default = false, defines = "TSOM_CLIENTLIB_STATIC"})
@@ -68,7 +29,8 @@ set_targetdir("./bin/$(plat)_$(arch)_$(mode)")
 
 if is_plat("windows") then
 	set_runtimes(is_mode("debug") and "MDd" or "MD")
-	add_defines("NOMINMAX", "WIN32_LEAN_AND_MEAN")
+	add_cxflags("/bigobj", "/permissive-", "/Zc:__cplusplus", "/Zc:externConstexpr", "/Zc:inline", "/Zc:lambda", "/Zc:preprocessor", "/Zc:referenceBinding", "/Zc:strictStrings", "/Zc:throwingNew", {tools = "cl"})
+	add_defines("_CRT_SECURE_NO_WARNINGS", "_ENABLE_EXTENDED_ALIGNED_STORAGE", "NOMINMAX", "WIN32_LEAN_AND_MEAN")
 end
 
 target("CommonLib", function ()
