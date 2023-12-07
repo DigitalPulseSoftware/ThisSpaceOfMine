9fd770eb023bc186eb6a454d6a74379793ffd777
SirLynix <lynix680@gmail.com>
2023-12-07 16:57:18 +0100

    Game: Fix connecting status not centered

--- src/Game/States/ConnectionState.cpp
+++ src/Game/States/ConnectionState.cpp
@@ -152,6 +152,7 @@ namespace tsom
 	void ConnectionState::UpdateStatus(const Nz::AbstractTextDrawer& textDrawer)
 	{
 		m_connectingLabel->UpdateText(textDrawer);
+		m_connectingLabel->Resize(m_connectingLabel->GetPreferredSize());
 		m_connectingLabel->Center();
 		m_connectingLabel->Show();
 
