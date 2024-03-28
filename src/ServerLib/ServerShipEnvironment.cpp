bf111734833e2b9a8004559888486631fe0039a9
SirLynix <lynix680@gmail.com>
2024-03-29 00:11:06 +0100

    Fix copyright notices and header guards

--- src/ServerLib/ServerShipEnvironment.cpp
+++ src/ServerLib/ServerShipEnvironment.cpp
@@ -1,6 +1,6 @@
 // Copyright (C) 2024 Jérôme "SirLynix" Leclercq (lynix680@gmail.com)
 // This file is part of the "This Space Of Mine" project
-// For conditions of distribution and use, see copyright notice in Config.hpp
+// For conditions of distribution and use, see copyright notice in LICENSE
 
 #include <ServerLib/ServerShipEnvironment.hpp>
 #include <CommonLib/ChunkEntities.hpp>
