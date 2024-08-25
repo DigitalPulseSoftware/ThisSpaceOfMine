3f701f827809770b05e37f892b38dcc74c755160
SirLynix <lynix680@gmail.com>
2024-08-25 15:24:32 +0200

    Main: Enable line-buffering for all platforms

--- src/Main/Main.cpp
+++ src/Main/Main.cpp
@@ -7,18 +7,34 @@
 #include <CommonLib/Utility/CrashHandler.hpp>
 #include <fmt/color.h>
 #include <fmt/format.h>
+#include <cstdio>
 #include <exception>
 
 #ifdef NAZARA_PLATFORM_WINDOWS
 #include <Windows.h>
+#else
+#include <sys/stat.h>
+#include <unistd.h>
 #endif
 
 int TSOMEntry(int argc, char* argv[], int(*mainFunc)(int argc, char* argv[]))
 {
+	// Enable line-buffering for stdout
+	std::size_t buffSize = BUFSIZ;
+#ifdef NAZARA_PLATFORM_POSIX
+	// try to get system preferred stdout blocksize
+	if (struct stat stat; fstat(STDOUT_FILENO, &stat) == 0)
+		buffSize = stat.st_blksize;
+	else
+		fmt::print(fg(fmt::color::yellow), "failed to fstat stdout ({})", errno);
+#endif
+
+	std::setvbuf(stdout, nullptr, _IOLBF, buffSize);
+
 	fmt::print(fg(fmt::color::white), "TSOM {0}.{1}.{2} {3} ({4}) - {5}\n", tsom::GameMajorVersion, tsom::GameMinorVersion, tsom::GamePatchVersion, tsom::BuildBranch, tsom::BuildCommit, tsom::BuildCommitDate);
 
 #ifdef NAZARA_PLATFORM_WINDOWS
-	if (IsDebuggerPresent())
+	if (::IsDebuggerPresent())
 		return mainFunc(argc, argv);
 #endif
 
