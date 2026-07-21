7d4057788c9aa37604b9bc2a53104d528ee75236
SirLynix <lynix680@gmail.com>
2026-07-21 17:11:05 +0200

    Rebase fixes

--- scripts/assets/solar_panel.lua
+++ scripts/assets/solar_panel.lua
@@ -1,4 +1,4 @@
-local primitive = Primitive.Box(Vec3f(2.0, 0.1, 2.0))
+local primitive = Primitive.Box(Vec3(2.0, 0.1, 2.0))
 
 local mesh = Mesh.CreateStatic()
 mesh:BuildSubMesh(primitive)
@@ -7,7 +7,7 @@ mesh:SetMaterialCount(1)
 local model = Model.BuildFromMesh(mesh)
 
 local solarPanelMat = MaterialInstance.Instantiate(MaterialType.PhysicallyBased)
-solarPanelMat:SetTextureProperty("BaseColorMap", Texture.Load("assets/dev/grey.png"))
+solarPanelMat:SetTextureProperty("BaseColorMap", Texture.Load("CookedAssets/Textures/Dev/grey.dds"))
 solarPanelMat:SetValueProperty("BaseColor", Color(0.2, 0.2, 1.0))
 
 model:SetMaterial(0, solarPanelMat)
