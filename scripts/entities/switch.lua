72b101a97d99d1bad5166568689c79d16e5a7f37
SirLynix <lynix680@gmail.com>
2026-07-21 23:35:06 +0200

    Add switch entity

---
+++ scripts/entities/switch.lua
@@ -0,0 +1,57 @@
+local classData = EntityRegistry.ClassBuilder()
+classData:Set("spawnable", true)
+classData:Set("spawnable_model", "switch")
+classData:Set("spawnable_collider", Vec3(0.25))
+
+classData:AddProperty("enabled", { type = "bool", default = true, isNetworked = true })
+
+classData:On("init", function (self)
+	local physSettings = {
+		kind = "static",
+		mass = 0.0,
+		collider = BoxCollider3D.new(Vec3(0.25)),
+		objectLayer = Constants.ObjectLayerStatic
+	}
+
+	self:AddComponent("rigidbody3d", physSettings)
+	self:SetInteractible(true)
+
+	if SERVER then
+		local distribution = self:AddComponent("distribution", {
+			inputs = { DistributionType.Electrical },
+			outputs = { DistributionType.Electrical }
+		})
+		distribution:UpdateConsumptionValue(0, Distribution.ToTickUnit(1000))
+	elseif CLIENT then
+		self:SetInteractibleText("Switch (enabled)")
+
+		local model = AssetLibrary.GetModel("switch")
+
+		local gfx = self:AddComponent("graphics")
+		gfx:AttachRenderable(model, Constants.RenderMask3D)
+	end
+end)
+
+if SERVER then
+	classData:On("interact", function (self, player)
+		self:UpdateProperty("enabled", not self:GetProperty("enabled"))
+		if not self:GetProperty("enabled") then
+			-- TODO: Disable tick when not enabled
+			local distribution = self:GetComponent("distribution")
+			distribution:UpdateProductionValue(0, 0)
+		end
+	end)
+
+	classData:On("distribution", function (self)
+		if self:GetProperty("enabled") then
+			local distribution = self:GetComponent("distribution")
+			local electricity = distribution:GetDistributedValue(DistributionType.Electrical)
+			distribution:UpdateProductionValue(0, electricity)
+		end
+	end)
+else
+	classData:OnPropertyUpdate("enabled", function (self, isEnabled)
+		self:SetInteractibleText(isEnabled and "Switch (enabled)" or "Switch (disabled)")
+	end)
+end
+EntityRegistry.RegisterClass("switch", classData)
