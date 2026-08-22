12158ca46a32278f770db27fcb302faf4106a3e1
SirLynix <lynix680@gmail.com>
2026-08-23 00:58:08 +0200

    Fix plant_pot metalness

--- scripts/assets/plant_pot.lua
+++ scripts/assets/plant_pot.lua
@@ -13,7 +13,7 @@ local material = MaterialInstance.Instantiate(MaterialType.PhysicallyBased)
 material:SetTextureProperty("BaseColorMap", Texture.Load("CookedAssets/Models/PlantPot/Textures/BaseColor.dds"))
 material:SetTextureProperty("NormalMap", Texture.Load("CookedAssets/Models/PlantPot/Textures/Normal.dds"))
 material:SetTextureProperty("RoughnessMap", Texture.Load("CookedAssets/Models/PlantPot/Textures/Roughness.dds"))
-material:SetValueProperty("Metallic", 0.0)
+material:SetValueProperty("MetallicFactor", 0.0)
 
 plantPot:SetMaterial(0, material)
 plantPot:SetMaterial(1, material)
