63fdb429fdb5158d61cb0b154c3f6c66dfb0c00b
SirLynix <lynix680@gmail.com>
2026-09-07 13:53:38 +0200

    Update Version.cpp

--- src/CommonLib/Version.cpp
+++ src/CommonLib/Version.cpp
@@ -3,7 +3,7 @@
 // For conditions of distribution and use, see copyright notice in LICENSE
 
 #include <CommonLib/Version.hpp>
-#include <spdlog/spdlog.h>
+#include <fmt/format.h>
 
 namespace tsom
 {
